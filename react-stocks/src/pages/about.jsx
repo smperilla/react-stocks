32ba5ddd7aecb7bfbb0196c351db6426b2a44570
sperilla <smperilla@gmail.com>
2023-09-21 19:34:38 -0400

    Created pages and imported routes to home

---
+++ react-stocks/src/pages/about.jsx
@@ -0,0 +1,10 @@
+const About = (props) => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is an about page</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ react-stocks/src/pages/stocks.jsx
@@ -0,0 +1,9 @@
+const Stocks = (props) => {
+    return (
+        <div>
+        <h1>Stocks</h1>
+        <p>This is the stocks page</p>
+        </div>
+    );
+    }
+    export default Stocks;
